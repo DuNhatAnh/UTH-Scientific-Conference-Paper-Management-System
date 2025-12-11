15012767c08a782d22da38457d93991df60408fd
QuocBaoo <nguyenbao180905@gmail.com>
2025-12-11 21:17:38 +0700

    feat: setup Program.cs + add DbContext + enable Swagger

--- UTH-ConfMS-Backend/UTHConfMS.API/Program.cs
+++ UTH-ConfMS-Backend/UTHConfMS.API/Program.cs
@@ -1,41 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using UTHConfMS.Infra.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 1. Add DbContext (Kết nối SQL Server)
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 2. Add Controllers
+builder.Services.AddControllers();
+// 3. Add Swagger (API Docs)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+// 4. Use Swagger UI
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
+// 5. Middleware
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseAuthorization();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// 6. Map Controllers
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
